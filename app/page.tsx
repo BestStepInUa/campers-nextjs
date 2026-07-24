f6cd0626f14eb009408e03b8a296594c82d100d2
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-24 22:22:25 +0300

    remove shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1">
         <Hero />
