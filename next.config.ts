84a7f90404fcc782ec1acb496156ab73c744907d
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-24 18:40:08 +0300

    add fonts, main layout metadata, TanstackProvider

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "ftp.goit.study" },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
